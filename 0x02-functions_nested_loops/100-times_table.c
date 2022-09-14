08ff7185543aacc03751449f671bd1b7c68a267f
Dahcole <franklynnnolukachukwudalu@gmail.com>
2022-09-14 03:23:45 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,38 @@
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, j, product;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; ++i)
 		{
-			for (y = 0; y <= n; y++)
+			for (j = 0; j <= n; ++j)
 			{
-				z = x * y;
-				if (z > 99)
+				product = i * j;
+				if (j != 0)
 				{
 					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(' ');
+					if (product < 100)
+						_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
 				}
-				else if (z > 9)
+				if (product >= 100)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar(((z / 10) % 10) + '0');
+					_putchar('0' + product / 100);
+					_putchar('0' + (product % 100) / 10);
+					_putchar('0' + (product % 10));
+				}
+				else if (product <= 9)
+				{
+					_putchar('0' + product);
 				}
 				else
 				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
 				}
 			}
 			_putchar('\n');
