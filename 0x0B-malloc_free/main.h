625c2ad7d6349ecbd87a3da6188c591d7ff42ced
Dahcole <franklynnnolukachukwudalu@gmail.com>
2022-10-05 08:05:34 -0700

    3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
